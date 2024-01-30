8839c51f9cbaebc20df8aa2342c5c7a85e75acd8
dsarotti <sr.sarotti@gmail.com>
2024-01-30 17:51:26 +0100

    Añadida cabecera en el juego PalabraAleatoria mostrando descripción del juego

--- lib/actividades/partidas/palabra_aleatoria/actividad_palabra_aleatoria.dart
+++ lib/actividades/partidas/palabra_aleatoria/actividad_palabra_aleatoria.dart
@@ -7,8 +7,7 @@ import 'package:get/get.dart';
 
 class ActividadPalabraAleatoria extends StatelessWidget {
   ActividadPalabraAleatoria({super.key});
-  final ControladorPalabraAleatoria controlador =
-      Get.find<ControladorPalabraAleatoria>();
+  final ControladorPalabraAleatoria controlador = Get.find<ControladorPalabraAleatoria>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,19 +23,31 @@ class ActividadPalabraAleatoria extends StatelessWidget {
                 borderRadius: ComponentesEstaticos.borderRadiusContenedor,
               ),
               margin: ComponentesEstaticos.paddingDefault,
-              padding: const EdgeInsets.symmetric(vertical:10),
+              padding: const EdgeInsets.symmetric(vertical: 10),
               width: Get.width * 0.9,
               constraints: const BoxConstraints(
                 minWidth: 200,
               ),
-              child: const Text(
-                textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colores.primarioOscuro,
-                    fontSize: 40,
-                    shadows: [ComponentesEstaticos.sombraTextoFino],
-                  ),
-                  "Palabra aleatoria"),
+              child: const Column(
+                children: [
+                  Text(
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colores.primarioOscuro,
+                        fontSize: 40,
+                        shadows: [ComponentesEstaticos.sombraTextoFino],
+                      ),
+                      "Palabra aleatoria"),
+                  Text(
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colores.primarioOscuro,
+                        fontSize: 20,
+                        shadows: [ComponentesEstaticos.sombraTextoFino],
+                      ),
+                      "Adivina la palabra en el mínimo de intentos posibles."),
+                ],
+              ),
             ),
             Expanded(
               child: Obx(() {
@@ -48,10 +59,7 @@ class ActividadPalabraAleatoria extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.start,
                     verticalDirection: VerticalDirection.down,
                     children: [
-                      for (int i =
-                              controlador.palabrasIntroducidas.length - 1;
-                          i >= 0;
-                          i--)
+                      for (int i = controlador.palabrasIntroducidas.length - 1; i >= 0; i--)
                         Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -64,71 +72,41 @@ class ActividadPalabraAleatoria extends StatelessWidget {
                                 style: const TextStyle(
                                   fontSize: 40,
                                   color: Colores.primarioOscuro,
-                                  shadows: [
-                                    ComponentesEstaticos.sombraTextoFino
-                                  ],
+                                  shadows: [ComponentesEstaticos.sombraTextoFino],
                                 ),
                               ),
                             ),
-                            for (int j = 0;
-                                j <
-                                    controlador
-                                        .palabrasIntroducidas[i].length;
-                                j++)
+                            for (int j = 0; j < controlador.palabrasIntroducidas[i].length; j++)
                               Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 4.0, horizontal: 2),
+                                padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 2),
                                 child: Container(
                                   width: 40,
                                   height: 60,
                                   decoration: BoxDecoration(
-                                    borderRadius: ComponentesEstaticos
-                                        .borderRadiusContenedor,
-                                    boxShadow: [
-                                      ComponentesEstaticos
-                                          .sombraContainersDefault
-                                    ],
+                                    borderRadius: ComponentesEstaticos.borderRadiusContenedor,
+                                    boxShadow: const [ComponentesEstaticos.sombraContainersDefault],
 
                                     ///establece el color del fondo dependiendo del acierto o fallo
-                                    color: controlador
-                                                    .palabrasIntroducidas[i]
-                                                [j] ==
-                                            (j <
-                                                    controlador
-                                                        .palabraAleatoria
-                                                        .length
-                                                ? controlador
-                                                    .palabraAleatoria[j]
+                                    color: controlador.palabrasIntroducidas[i][j] ==
+                                            (j < controlador.palabraAleatoria.length
+                                                ? controlador.palabraAleatoria[j]
                                                 : null)
                                         ? Colores.acierto
-                                        : controlador.palabraAleatoria
-                                                .contains(controlador
-                                                        .palabrasIntroducidas[
-                                                    i][j])
+                                        : controlador.palabraAleatoria.contains(controlador.palabrasIntroducidas[i][j])
                                             ? Colores.aciertoParcial
                                             : Colores.fallo,
                                   ),
                                   child: Center(
                                     child: Text(
-                                      controlador.palabrasIntroducidas[i]
-                                          [j],
+                                      controlador.palabrasIntroducidas[i][j],
                                       textAlign: TextAlign.center,
                                       style: TextStyle(
                                         fontSize: 40,
 
                                         ///establece el color de esta letra fondo dependiendo del acierto o fallo
-                                        color: controlador
-                                                        .palabrasIntroducidas[
-                                                    i][j] ==
-                                                (j <
-                                                        controlador
-                                                            .palabraAleatoria
-                                                            .length
-                                                    ? controlador
-                                                        .palabraAleatoria[j]
-                                                    : null)
-                                            ? Get.theme.colorScheme
-                                                .inversePrimary
+                                        color: controlador.palabrasIntroducidas[i][j] ==
+                                                (j < controlador.palabraAleatoria.length ? controlador.palabraAleatoria[j] : null)
+                                            ? Get.theme.colorScheme.inversePrimary
                                             : Colores.primarioOscuro,
                                       ),
                                     ),
@@ -143,13 +121,9 @@ class ActividadPalabraAleatoria extends StatelessWidget {
               }),
             ),
             Padding(
-              padding: const EdgeInsets.only(bottom:12.0,right: 12,left: 12),
+              padding: const EdgeInsets.only(bottom: 12.0, right: 12, left: 12),
               child: TextField(
-                buildCounter: (BuildContext context,
-                        {int? currentLength,
-                        int? maxLength,
-                        bool? isFocused}) =>
-                    null,
+                buildCounter: (BuildContext context, {int? currentLength, int? maxLength, bool? isFocused}) => null,
                 autocorrect: false,
                 controller: controlador.palabraActual,
                 onSubmitted: (value) => controlador.submit(value),
