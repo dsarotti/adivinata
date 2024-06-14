a10b74dc300537e0cc5769f9a529ccb4fd785f53
dsarotti <sr.sarotti@gmail.com>
2024-06-14 11:44:54 +0200

    bugfix en historial

--- lib/actividades/misc/historial/controlador/controlador_historial.dart
+++ lib/actividades/misc/historial/controlador/controlador_historial.dart
@@ -1,5 +1,7 @@
 import 'package:adivinata/actividades/adivinata/servicio/adivinata_servicio.dart';
 import 'package:adivinata/model/partida.dart';
+import 'package:adivinata/model/usuario.dart';
+import 'package:adivinata/model/usuario_app.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../../res/colores.dart';
@@ -12,14 +14,22 @@ class ControladorHistorial extends GetxController{
   buildListaPartidas(List<Partida> partidas){
     for (Partida partida in partidas){
       widgetList.addAll ([
-            Text(
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colores.primarioOscuro,
-                  fontSize: 25,
-                  shadows: [ComponentesEstaticos.sombraTextoFino],
-                ),
-                "id partida: ${partida.id}"),
+        Text(
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colores.primarioOscuro,
+              fontSize: 25,
+              shadows: [ComponentesEstaticos.sombraTextoFino],
+            ),
+            "id partida: ${partida.id}"),
+        Text(
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colores.primarioOscuro,
+              fontSize: 25,
+              shadows: [ComponentesEstaticos.sombraTextoFino],
+            ),
+            "id usuario: ${partida.usuarioId}"),
             Text(
                 textAlign: TextAlign.center,
                 style: const TextStyle(
@@ -48,6 +58,11 @@ class ControladorHistorial extends GetxController{
   @override
   void onInit() async {
     List<Partida> partidas = await Get.find<AdivinataServicio>().dbHelper.getPartidasList();
+    for (int i = 0 ; i<partidas.length;i++){
+      if (partidas[i].usuarioId!=UsuarioApp().id){
+        partidas.removeAt(i);
+      }
+    }
     buildListaPartidas(partidas);
     super.onInit();
   }
